c8a7c4f7f88cc28084ae59c2017427b0ab1f3b60
Jimmy Monin <jimmy@monin.net>
2017-01-08 14:16:28 +0100

    Fix symbolic links to /home/yunohost.app

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,14 +8,21 @@ VERSION="2.8.5"
 # Remote URL to fetch application source archive
 APPLICATION_SOURCE_URL="http://piwigo.org/download/dlcounter.php?code=${VERSION}"
 
+#
+# Common helpers
+#
+
 # Download and extract application sources to the given directory
 # usage: extract_application_to DESTDIR
 extract_application() {
   local DESTDIR=$1
-  archive="${DESTDIR}/application.zip"
+  TMPDIR=$(mktemp -d)
+  chmod 755 $TMPDIR
+  archive="${TMPDIR}/application.zip"
   wget -q -O "$archive" "$APPLICATION_SOURCE_URL" \
-|| ynh_die "Unable to download application archive"
-  sudo unzip -qq "$archive" -d "$DESTDIR" \
+    || ynh_die "Unable to download application archive"
+  unzip -qq "$archive" -d "$TMPDIR" \
     || ynh_die "Unable to extract application archive"
   rm "$archive"
+  sudo rsync -a "$TMPDIR"/*/* "$DESTDIR"
 }
