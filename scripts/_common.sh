4adda14d459492fd474a5f139ede2728483c0e28
Jimmy Monin <jimmy@monin.net>
2017-01-28 12:42:43 +0100

    Fix upgrade when installed on domain root

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -27,3 +27,16 @@ extract_application() {
   rm "$archive"
   sudo rsync -a "$TMPDIR"/*/* "$DESTDIR"
 }
+
+# Fix path if needed
+# usage: fix_patch PATH_TO_FIX
+fix_path() {
+  local path=$1
+  if [ "${path:0:1}" != "/" ] && [ ${#path} -gt 0 ]; then
+         path="/$path"
+  fi
+  if [ "${path:${#path}-1}" == "/" ] && [ ${#path} -gt 1 ]; then
+         path="${path:0:${#path}-1}"
+  fi
+  echo "$path"
+}
