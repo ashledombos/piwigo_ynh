e683d22272e26f03d46b8bbeeaedc1b38e9f6425
Jimmy Monin <jimmy@monin.net>
2017-06-10 21:39:33 +0200

    Prevent checking for new updates

--- conf/config.inc.php
+++ conf/config.inc.php
@@ -6,5 +6,8 @@ $conf['apache_authentication'] = true;
 // Use external authentication
 $conf['external_authentification'] = true;
 
-
+// how often should we check for new versions of Piwigo on piwigo.org? In
+// seconds. The check is made only if there are visits on Piwigo.
+// 0 to disable.
+$conf['update_notify_check_period'] = 0;
 ?>
