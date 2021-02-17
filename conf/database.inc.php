5f7c0e4e308b627d67e2f0150ab70d8ad98452bc
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2021-02-17 18:17:11 +0100

    Upgrade to version 11.3.0 (#59)
    * Upgrade to version 11.3.0

--- conf/database.inc.php
+++ conf/database.inc.php
@@ -1,8 +1,8 @@
 <?php
 $conf['dblayer'] = 'mysqli';
-$conf['db_base'] = '__DBTOCHANGE__';
-$conf['db_user'] = '__USERTOCHANGE__';
-$conf['db_password'] = '__PASSTOCHANGE__';
+$conf['db_base'] = '__DB_NAME__';
+$conf['db_user'] = '__DB_NAME__';
+$conf['db_password'] = '__DB_PWD__';
 $conf['db_host'] = 'localhost';
 
 $prefixeTable = '';
