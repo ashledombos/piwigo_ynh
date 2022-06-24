c991a485802d1b0ed0d2cf31cd9c166af27bf946
yalh76 <yalh@yahoo.com>
2022-06-24 05:33:16 +0200

    Upgrade to 12.2.0~ynh1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,9 +7,7 @@
 YNH_PHP_VERSION="7.3"
 
 # dependencies used by the app
-pkg_dependencies="imagemagick"
-
-extra_php_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-mysqli"
+pkg_dependencies="imagemagick php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-mysqli"
 
 #=================================================
 # PERSONAL HELPERS
