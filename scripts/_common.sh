68f1616496d467ba3e9e9f7d6bc1fb2efee04673
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-10-05 21:48:46 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION="8.0"
-
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="imagemagick php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-ldap"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
