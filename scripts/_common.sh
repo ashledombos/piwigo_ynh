a1d68a915c3b53dff45db15dd132538ab201eb7b
Félix Piédallu <felix@piedallu.me>
2024-03-23 00:00:04 +0100

    Uniformize data_dir creation

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,16 @@
 # PERSONAL HELPERS
 #=================================================
 
+_create_data_dirs_if_required() {
+    dirs=(_data galleries upload)
+    for dir in "${dirs[@]}"; do
+        if [ ! -d "$data_dir/$dir" ]; then
+            rsync -a "$install_dir/$dir/" "$data_dir/$dir/"
+        fi
+        ynh_secure_remove "$install_dir/$dir"
+    done
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
